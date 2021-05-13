b17f1de0b51c083d9020f2b8ca215b8e95acc9bf
Kelly <78561980+kelbrisel@users.noreply.github.com>
2021-05-13 17:08:37 -0500

    comments

--- static/js/app.js
+++ static/js/app.js
@@ -3,14 +3,21 @@ const tableData = data;
 // Reference the HTML table using d3
 var tbody= d3.select("tbody");
 function buildTable(data){
+    // First clear out any existing data
     tbody.html(" ");
-}
-data.forEach((dataRow) => {
-    let row = tbody.append("tr");
-    Object.values(dataRow).forEach(val) =>{
-        lew cell = row.append("td");
-        cell.text(val);
-        }
-    );
-});
 
+    // Next, loop through each object in the data
+    //and append a row and cells for each value in the row
+    data.forEach((dataRow) => {
+        // append a row to thr table body
+        let row = tbody.append("tr");
+
+        //Loop through each field in the dataRow and add
+        //each value as a table cell (tD)
+        Object.values(dataRow).forEach(val) => {
+            let cell = row.append("td");
+            cell.text(val);
+            }   
+         );
+    });
+}
